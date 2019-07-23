9aaf8bcb8262f69e4079d40d8b85aa7427d341e4
tabio <tabio.github@gmail.com>
2019-07-24 08:06:20 +0900

    check empty

--- slideshare_search_to_slack.rb
+++ slideshare_search_to_slack.rb
@@ -16,14 +16,15 @@ keywords << { query: 'hoge', channel: 'piyo' }
 
 keywords.each do |keyword|
   message = []
-  message << '-' * 30
   response_objects = client.search(keyword[:query])
 
   response_objects.each do |obj|
+    message << '-' * 30
     message << "*#{obj.title}*"
     message << "表示回数: #{obj.num_views}"
     message << "作成日: #{obj.created.strftime('%Y-%m-%d')}"
     message << obj.url
   end
-  notifier.ping message.join("\n"), channel: "##{keyword[:channel]}"
+
+  notifier.ping message.join("\n"), channel: "##{keyword[:channel]}" unless message.empty?
 end
