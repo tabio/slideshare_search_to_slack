4a0f3c5695549bcac849c24d044fd1b6cf652c25
tabio <tabio.github@gmail.com>
2019-07-23 22:25:09 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'nokogiri'
+gem 'slack-notifier'
+gem 'slideshare_search', git: 'https://github.com/tabio/slideshare_search'
---
+++ slideshare_search_to_slack.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'slideshare_search'
+require 'slack-notifier'
+
+WEBHOOK_URL = ''
+API_KEY     = ''
+SECRET_KEY  = ''
+
+notifier = Slack::Notifier.new WEBHOOK_URL
+date = (Date.today - 1).to_s
+client = SlideshareSearch::Client.new(API_KEY, SECRET_KEY, date)
+
+keywords = []
+keywords << { query: 'hoge', channel: 'piyo' }
+
+keywords.each do |keyword|
+  message = []
+  message << '-' * 30
+  response_objects = client.search(keyword[:query])
+
+  response_objects.each do |obj|
+    message << "*#{obj.title}*"
+    message << "表示回数: #{obj.num_views}"
+    message << "作成日: #{obj.created.strftime('%Y-%m-%d')}"
+    message << obj.url
+  end
+  notifier.ping message.join("\n"), channel: "##{keyword[:channel]}"
+end
